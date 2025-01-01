f3bb60a424e4d76f2b75e619e263a88699ef5aa6
Miss-Siddhi-Pagire <siddhipagire825@gmail.com>
2025-01-01 13:34:11 +0530

    component created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello</h1>)
+function Greeting(){
+    return (
+        <div>
+            <h1>hello sakshi {Math.random()}</h1>
+        </div>
+
+    );   
+}
+root.render
+(   <>
+        <h1>Hello</h1>
+        <h2>hello wordld {Math.random()}</h2>
+        <Greeting/>
+    </>
+)
